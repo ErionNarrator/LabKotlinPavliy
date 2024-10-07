26023e3c94598f72eae35199d6e0e4ca2debcf4b
ErionNarrator <144333809+ErionNarrator@users.noreply.github.com>
2024-10-07 18:31:20 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -6,27 +6,21 @@ fun main() {
     //домов проведена подряд. Дома с нечетными номерами расположены на од-
     //ной стороне улицы, с четными — на другой. На какой стороне улицы проживает больше жителей?
 
-//    val home = arrayOf(1, 2, 3, 4, 5, 6, 7)
-//    val oddHome = mutableListOf<Int>() // Для изменения списка
-//    val evenHome = mutableListOf<Int>() // Для изменения списка
-//    for (even in home) {
-//        if (even % 2 == 0) {
-//            evenHome.add(even)
-//        }
-//    }
-//    for  (odd in home) {
-//        if (odd %2 !=0){
-//            oddHome.add(odd)
-//        }
-//    }
-//
-//    val odd1 = oddHome.size
-//    val even1 = evenHome.size
-//    when {odd1 > even1 ->
-//    println("Нечетных больше")
-//    odd1 < even1 ->
-//    println("Четных больше")}
-//    println("$oddHome не четные, $evenHome четные")
+   val size = 10
+    val residents = IntArray(size) { Random.nextInt(1, 10) }
+    var oddSum = 0
+    var evenSum = 0
+    for (i in residents.indices)
+    {     if (i % 2 == 0) {
+        oddSum += residents[i]     }
+    else {
+        evenSum += residents[i]     }
+    }
+    when {     
+        oddSum > evenSum -> println("На нечетной стороне больше жителей.")
+        evenSum > oddSum -> println("На четной стороне больше жителей.")    
+        else -> println("На обеих сторонах одинаковое количество жителей.")
+    }
     //12.59. В зрительном зале 23 ряда, в каждом из которых 40 мест (кресел). Информа-
     //ция о проданных билетах хранится в двумерном массиве, номера строк кото-
     //рого соответствуют номерам рядов, а номера столбцов — номерам мест. Ес-
